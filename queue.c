bc55ee80fc496d4d8a67602d7129f070cbadf54e
chandler <chandler.kcyeh@gmail.com>
2023-12-21 20:11:37 +0800

    Correct q_remove_tail edge case check

--- queue.c
+++ queue.c
@@ -89,7 +89,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || list_empty(head))
+    if (!head || list_empty(head) || !sp)
         return NULL;
 
     struct list_head *list_node = head->prev;
